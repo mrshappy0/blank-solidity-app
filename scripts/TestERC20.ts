f7ba9471ea2ec11d2d699d826ae8daadb6b0be16
mrshappy0 <51244673+mrshappy0@users.noreply.github.com>
2022-11-14 15:12:12 -0700

    fetch minter role

--- scripts/TestERC20.ts
+++ scripts/TestERC20.ts
@@ -1,7 +1,5 @@
 import { ethers } from "hardhat";
 
-const MINTER_ROLE = "0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6";
-
 async function main() {
   const accounts = await ethers.getSigners();
   const tokenContractFactory = await ethers.getContractFactory("MyToken");
